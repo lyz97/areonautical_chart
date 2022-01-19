024307abb8891eabee425386d3e71e6d51f599c4
lyz97 <956325538@qq.com>
2022-01-19 22:30:29 +0800

    加入升降带 字体修改为TIMES NEW ROMAN 完成字体与跑道方向平行

--- runway_plot.py
+++ runway_plot.py
@@ -28,11 +28,10 @@ def plot_scale(times):
         t.forward(5)
         t.left(90)
 
-
-a = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)  # 获得屏幕分辨率X轴
-b = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)  # 获得屏幕分辨率Y轴
-print(a, b)
-t.write('1200×30', font=('Times New Rome', 40, 'normal'))
+# a = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)  # 获得屏幕分辨率X轴
+# b = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)  # 获得屏幕分辨率Y轴
+# print(a, b)
+# t.write('1200×30', font=('Times New Rome', 40, 'normal'))
 t.bgcolor('white')
 plot_scale(4)
 ts = t.getscreen()
--- test3.py
+++ test3.py
@@ -5,6 +5,7 @@ import turtle as t
 import math
 import pandas as pd
 import os
+import time
 
 
 def get_data(f):
@@ -32,7 +33,7 @@ def process_data(df):
 
     # -----------------------------------升降带-----------------------------------------------
     df4 = df['升降带'].str.extract(r'(\d+.).(\d+)')
-    df4['升降带'] = df2.apply(lambda x: x[0] + x[1], axis=1)
+    df4['升降带'] = df4.apply(lambda x: x[0] + x[1], axis=1)
     df['升降带'] = df4['升降带']
 
     return df
@@ -45,6 +46,9 @@ def plot_data(info):
     :param info: 一个机场的所有相关信息
     :return:
     """
+    rotation_rate = 360 - info['真方位1'] + 90
+    # 打印的标题距离图片的上方的距离
+    top_dis = 0.15
 
     # ---------------------------表面类型和跑道尺寸-----------------------------
     fig, ax = plt.subplots()
@@ -52,7 +56,11 @@ def plot_data(info):
     ax.axis('off')
 
     surface_scale = str(info['跑道尺寸']) + '   ' + str(info['表面类型'])
-    plt.title(str(surface_scale))
+
+    # 防止出现显示不全的情况
+    plt.gcf().subplots_adjust(top=top_dis)
+
+    plt.title(str(surface_scale), rotation=rotation_rate)
 
     plt.savefig('../pythonProject/airport_data/{}/{}.eps'.format(info['机场名'], '跑道尺寸和表面类型'), format='eps',
                 dpi=1000)
@@ -62,32 +70,43 @@ def plot_data(info):
     fig.patch.set_alpha(0.)
     ax.axis('off')
 
-    plt.title(str(info['跑道长度']))
+    # 防止出现显示不全的情况
+    plt.gcf().subplots_adjust(top=top_dis)
+
+    plt.title(str(info['跑道长度']), rotation=rotation_rate)
 
     plt.savefig('../pythonProject/airport_data/{}/{}.eps'.format(info['机场名'], '跑道长度'), format='eps', dpi=1000)
 
     plt.close()
 
     # ---------------------------升降带-----------------------------
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5, 5))
     fig.patch.set_alpha(0.)
     ax.axis('off')
 
     strip = 'Strip  ' + str(info['升降带'])
-    plt.title(strip)
+
+    # 防止出现显示不全的情况
+    plt.gcf().subplots_adjust(top=top_dis)
+
+    plt.title(strip, rotation=rotation_rate)
 
     plt.savefig('../pythonProject/airport_data/{}/{}.eps'.format(info['机场名'], '升降带'), format='eps', dpi=1000)
 
     plt.close()
 
     # ---------------------------机场标高-----------------------------
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5, 5))
     fig.patch.set_alpha(0.)
     ax.axis('off')
 
     try:
         elev = 'ELEV  ' + str(int(info['机场标高']))
-        plt.title(elev)
+
+        # 防止出现显示不全的情况
+        plt.gcf().subplots_adjust(top=top_dis)
+
+        plt.title(elev, rotation=rotation_rate)
     except ValueError:
         pass
 
@@ -101,7 +120,10 @@ def plot_data(info):
     ax.axis('off')
 
     try:
-        plt.title(str(int(info['跑道号1'])))
+        # 防止出现显示不全的情况
+        plt.gcf().subplots_adjust(top=top_dis)
+
+        plt.title(str(int(info['跑道号1'])), rotation=rotation_rate)
     except ValueError:
         pass
 
@@ -115,6 +137,9 @@ def plot_data(info):
     ax.axis('off')
 
     try:
+        # 防止出现显示不全的情况
+        plt.gcf().subplots_adjust(top=top_dis)
+
         plt.title(str(info['四字码']))
     except ValueError:
         pass
@@ -144,16 +169,27 @@ def plot_data(info):
     #     plt.close()
 
 
-def plot_runway(name, runway_scale, times, rotate, length, runway_path):
+def plot_runway(name, runway_scale, times, rotate, strip_scale, runway_path):
     """
 
     :param runway_path: 保存路径
-    :param length: 虚线宽度
+    :param strip_scale: 升降带尺寸
     :param runway_scale: 跑道尺寸，如'450x20'
     :param rotate: 跑道旋转度数
 
     :return:
     """
+    def dotted_line(l):
+        for i in range(math.ceil(l / 10)):
+            t.forward(5)
+            t.penup()
+            t.forward(5)
+            t.pendown()
+
+    def complete_dot(l):
+        for i in range(math.ceil(l/10)):
+            t.forward(10)
+
     t.reset()
     t.hideturtle()
     t.tracer(False)  # 不显示画图过程
@@ -162,8 +198,12 @@ def plot_runway(name, runway_scale, times, rotate, length, runway_path):
     # 确认缩放倍数
     try:
         a, b = runway_scale.split('×')
-        a = float(a) / (1.1 ** times)
-        b = float(b) / (1.1 ** times)
+        a = math.ceil(float(a) / (1.1 ** times))
+        b = math.ceil(float(b) / (1.1 ** times))
+
+        m, n = strip_scale.split('×')
+        m = float(m) / (1.1 ** times)
+        n = float(n) / (1.1 ** times)
     except AttributeError:
         print('{}信息缺失'.format(name))
         return
@@ -179,75 +219,59 @@ def plot_runway(name, runway_scale, times, rotate, length, runway_path):
     t.pendown()  # 放下画笔
 
     # --------------------------------画矩形---------------------------
-    t.forward(a / 2)  # 前进a像素
+    dotted_line(a / 2)  # 前进a像素
     t.left(90)  # 箭头左转90度
-    t.forward(b)  # 前进b像素
+    dotted_line(b)  # 前进b像素
     t.left(90)  # 箭头左转90度
-    t.forward(a)  # 前进a像素
+    dotted_line(a)  # 前进a像素
     t.left(90)  # 箭头左转90度
-    t.forward(b)  # 前进b像素
+    dotted_line(b)  # 前进b像素
     t.left(90)
-    t.forward(a / 2)
+    dotted_line(a / 2)
     t.end_fill()
 
-    # --------------------------------画虚线-----------------------------
-    def dotted_line(l):
-        for i in range(math.ceil(l / 6)):
-            t.forward(3)
-            t.penup()
-            t.forward(3)
-            t.pendown()
+    # -----------------------------升降带-----------------------------
+    x = (m - a) / 2
+    y = (n - b) / 2
 
-    def center_line(l):
-        for i in range(math.ceil(l/14)):
-            t.forward(7)
-            t.penup()
-            t.forward(7)
-            t.pendown()
+    x = math.ceil(x)
+    y = math.ceil(y)
 
-    # t.forward(length)
-    # t.left(90)
-    # t.forward(a)
-    # t.left(90)
-    # t.forward(b + 2 * length)
-    # t.left(90)
-    # t.forward(a)
-    # t.left(90)
-    # t.forward(b + 2 * length)
     t.left(180)
     t.penup()
-    t.forward(a / 2)
+    complete_dot(a/2)
+    complete_dot(x)
     t.left(90)
     t.pendown()
-
-    dotted_line(length)
-
+    dotted_line(y)
     t.left(90)
+
+    # 分开画防止四舍五入
+    dotted_line(x)
     dotted_line(a)
+    dotted_line(x)
     t.left(90)
-
-    # 防止四舍五入后虚线不一样长
-    dotted_line(length)
-    t.penup()
-    t.forward(b)
-    t.pendown()
-    dotted_line(length)
-
+    dotted_line(y)
+    # dotted_line(b)
+    dotted_line(b)
+    dotted_line(y)
+    t.pensize(1)
     t.left(90)
+    dotted_line(x)
     dotted_line(a)
+    dotted_line(x)
     t.left(90)
+    dotted_line(y)
+    dotted_line(b)
 
-    # 防止四舍五入后虚线不一样长
-    dotted_line(length)
-
-    # 跑道中线
-    t.penup()
-    t.forward(b/2)
-    t.pendown()
-    t.left(90)
-    t.pensize(2)
-    t.pencolor('white')
-    center_line(a)
+    # ------------------------跑道中线------------------------
+    # t.penup()
+    # t.forward(b/2)
+    # t.pendown()
+    # t.left(90)
+    # t.pensize(2)
+    # t.pencolor('white')
+    # center_line(a)
 
     t.hideturtle()
     ts = t.getscreen()
@@ -318,7 +342,7 @@ def plot_scale(ture_distance_per_pix, save_path):
 
 
 if __name__ == '__main__':
-    filename = 'D:/ZY数据.csv'
+    filename = 'D:/ZY数据1.csv'
     data = process_data(get_data(filename))
     pic = '../pythonProject/[3.1]阿荣通用机场.jpg'
     if os.path.isdir('../pythonProject/airport_data'):
@@ -333,7 +357,7 @@ if __name__ == '__main__':
 
     # for index, row in data.iterrows():
     #     print(index)
-
+    plt.rc('font', family='Times New Roman')
     for row_index, row in data.iterrows():
         if os.path.isdir('../pythonProject/airport_data/{}'.format(row['机场名'])):
             pass
@@ -347,7 +371,8 @@ if __name__ == '__main__':
         rotate = row['真方位1']
 
         times = evaluate_size(row['跑道长度'], row['真方位1'])
-        plot_runway(row['机场名'], runway_scale, times, rotate, 20, runway_path)
+        strip_scale = row['升降带']
+        plot_runway(row['机场名'], runway_scale, times, rotate, strip_scale, runway_path)
         scaling_bar_save_path = '../pythonProject/airport_data/{}/scaling.eps'.format(row['机场名'])
         plot_scale(times, scaling_bar_save_path)
 
